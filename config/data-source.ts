d164ed3e8cbed061a815033cb1adb4d0a40ad59a
Aliarrrzz <aliarzvi@gmail.com>
2026-05-25 20:32:40 +0330

    ch

--- config/data-source.ts
+++ config/data-source.ts
@@ -14,7 +14,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || '',
-  synchronize: process.env.NODE_ENV === 'development',
+  synchronize: true,
   logging: false,
   entities: [User, Channel, Message, VoiceSession],
 });
